53a989ea3ce5d64fe40e7e126e241389bfe74a8e
tofosky <adedejitofoski@gmail.com>
2022-10-14 19:29:27 +0100

    project created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { map, Observable, shareReplay } from 'rxjs';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+
+
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +12,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'moa-aqua-feed';
+
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+  .pipe(
+    map(result => result.matches),
+    shareReplay()
+  );
+
+constructor(private breakpointObserver: BreakpointObserver) {}
+log(){
+//this.authenticationService.logOut();
+}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,52 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { RouterModule} from '@angular/router';
+import {MatButtonModule} from '@angular/material/button';
+import {MatListModule} from '@angular/material/list';
+import {MatIconModule} from '@angular/material/icon';
+
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// import { AngularFireModule } from '@angular/fire';
+// import { AngularFirestoreModule } from '@angular/fire/firestore';
+// import { AngularFireStorageModule } from '@angular/fire/storage';
+// import { AngularFireAuthModule } from '@angular/fire/auth';
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDj-45eOO3TrVkEV8Ecp9WWxmxcrlQX5Yg",
+//   authDomain: "moa-aqua-feed.firebaseapp.com",
+//   projectId: "moa-aqua-feed",
+//   storageBucket: "moa-aqua-feed.appspot.com",
+//   messagingSenderId: "750942417241",
+//   appId: "1:750942417241:web:47424d7022dc5dfd46dc48",
+//   measurementId: "G-NZYWHWXDSX"
+// };
+
+
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    RouterModule,
+    MatButtonModule,
+    MatListModule,
+    MatIconModule
+    // AngularFirestoreModule, // firestore
+    // AngularFireAuthModule, // auth
+    // AngularFireStorageModule
   ],
   providers: [],
   bootstrap: [AppComponent]
